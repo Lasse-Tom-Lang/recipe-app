bbd1e339459843b68ffa27cfa71ad90d57586995
Lasse Lang <lang.lasse@gmx.de>
2022-10-31 19:58:23 +0100

    Initial Commit

---
+++ recipe-app/recipe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_appApp.swift
+//  recipe-app
+//
+//  Created by Lasse Lang on 31.10.22.
+//
+
+import SwiftUI
+
+@main
+struct recipe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
